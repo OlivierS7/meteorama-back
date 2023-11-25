1ad89ced309eb1758b5d5c63d53a854886eae6e9
OlivierS7 <olivier.simonneau@viacesi.fr>
2023-11-25 17:12:23 +0100

    Endpoint to get cities data

---
+++ app/Controllers/Http/CitiesController.ts
@@ -0,0 +1,16 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { prisma } from '@ioc:Adonis/Addons/Prisma'
+import Cache from '@ioc:Kaperskyguru/Adonis-Cache'
+
+export default class CitiesController {
+  public async index({ response }: HttpContextContract) {
+    try {
+      const cities = await Cache.remember('cities', 60, async function () {
+        return await prisma.cities.findMany()
+      })
+      return response.status(200).json(cities)
+    } catch (error) {
+      return response.status(500).json(error)
+    }
+  }
+}
